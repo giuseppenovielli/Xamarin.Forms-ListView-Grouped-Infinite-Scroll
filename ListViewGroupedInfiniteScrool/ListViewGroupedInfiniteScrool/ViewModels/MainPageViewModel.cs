d868175111ddb5e926e528ed478c5f33b51fc4c8
Hrcoffee2 <giuseppe.novielli@hrcoffee.it>
2021-06-13 14:18:31 +0200

    first commit

---
+++ ListViewGroupedInfiniteScrool/ListViewGroupedInfiniteScrool/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ListViewGroupedInfiniteScrool.ViewModels
+{ 
+    public class MainPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<ItemGroupViewModel> ItemsList { get; set; }
+        public bool IsRefresh { get; set; }
+        public bool IsLoadingMore { get; set; }
+
+        public ICommand RefreshListView { get; set; }
+
+        public MainPageViewModel()
+        {
+            ItemsList = new ObservableCollection<ItemGroupViewModel>();
+
+            RefreshListView = new Command(execute: async (obj) =>
+            {
+                ItemsList.Clear();
+
+                await GetItems();
+
+
+            });
+
+            _ = GetItems();
+        }
+
+        public async Task GetItems()
+        {
+            await Task.Delay(2000);
+
+            System.Diagnostics.Debug.WriteLine("\nGetItems");
+
+            //Estraggo l'ultimo elemento dalla lista
+            var lastItemNum = 0;
+            if (ItemsList.Count > 0)
+            {
+                var lastItemGrouped = ItemsList[ItemsList.Count - 1];
+                if (lastItemGrouped != null)
+                {
+                    var lastItem = lastItemGrouped[lastItemGrouped.Count - 1];
+                    if (lastItem != null)
+                    {
+                        lastItemNum = lastItem.Num;
+                        lastItemNum += 1;
+                    }
+                }
+
+                IsLoadingMore = true;
+            }
+            else
+            {
+                IsRefresh = true;
+            }
+
+
+            for (int i = lastItemNum; i < lastItemNum + 10; i++)
+            {
+                var result = i / (double)10;
+                System.Diagnostics.Debug.WriteLine($"result = {result}     i = {i}");
+
+                if (result % 1 == 0)
+                {
+                    var numList = new ObservableCollection<ItemViewModel>
+                    {
+                        InstanceCell(i+1)
+                    };
+                    
+                    var itemGrouped = new ItemGroupViewModel(i.ToString(), i.ToString(), numList);
+
+                    ItemsList.Add(itemGrouped);
+
+                    i += 1;
+                }
+                else
+                {
+                    var lastGroupedList = ItemsList[ItemsList.Count - 1];
+                    lastGroupedList.Add(InstanceCell(i));
+                }
+            }
+
+            IsLoadingMore = false;
+            IsRefresh = false;
+        }
+
+        ItemViewModel InstanceCell(int num)
+        {
+            return new ItemViewModel
+            {
+                Num = num
+            };
+        }
+    }
+
+    public class ItemGroupViewModel : ObservableCollection<ItemViewModel>
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+
+
+        public ItemGroupViewModel(string name, string shortName, ObservableCollection<ItemViewModel> listCellVM) : base(listCellVM)
+        {
+            Name = name;
+            ShortName = shortName;
+        }
+    }
+
+    public class ItemViewModel
+    {
+        public int Num { get; set; }
+    }
+}
---
+++ ListViewGroupedInfiniteScrool/ListViewGroupedInfiniteScrool/Views/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using ListViewGroupedInfiniteScrool.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace ListViewGroupedInfiniteScrool
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+
+            On<iOS>().SetUseSafeArea(true);
+        }
+
+        async void listView_ItemAppearing(System.Object sender, Xamarin.Forms.ItemVisibilityEventArgs e)
+        {
+            var cellVM = BindingContext as MainPageViewModel;
+            if (cellVM != null && e.Item != null)
+            {
+
+                var itemsList = cellVM.ItemsList;
+                var itemsListCount = itemsList.Count;
+
+                var itemGroup = e.Item as ItemViewModel;
+
+
+                if (itemsList != null &&
+                    itemsList.Count > 0 &&
+                    itemGroup != null
+                    )
+                {
+
+                    var lastGroupedItem = itemsList[itemsListCount - 1];
+                    if (lastGroupedItem.Count > 0)
+                    {
+                        var lastItem = lastGroupedItem[lastGroupedItem.Count - 1];
+                        if (itemGroup.Num == lastItem.Num)
+                        {
+                            System.Diagnostics.Debug.WriteLine($"\n\nLoadingMore lastItem.Num = {lastItem.Num}");
+                            System.Diagnostics.Debug.WriteLine($"LoadingMore itemGroup.Num = {itemGroup.Num}");
+
+                            await cellVM.GetItems();
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
